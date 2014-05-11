191f5ed562068204fc14fb242b44c6568b7a65ae
Cornel van der Heiden <cornel@nenicto.com>
2014-05-11 19:13:51 +0200

    Base classes added, Crypto stub created.

---
+++ lib/BaseSettings.lib.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Define our database host.
+ */
+define("DB_HOST", "localhost");
+/**
+ * Set the user permitted to access the DB_NAME schema.
+ */
+define("DB_USER", "root");
+/**
+ * Set our database password.
+ */
+define("DB_PASS", "Q!W@E#");
+/**
+ * Set the default schema (database).
+ */
+define("DB_NAME", "test");
+/**
+ * At the end of this definitions file, we tell the application we are loaded.
+ */
+define("SETTINGS_LOADED", true);
